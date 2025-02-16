fc9882dd042da1cb740260a55d5096695aa4a28e
sungbinm <a01039962802@khu.ac.kr>
2025-02-16 17:26:43 +0900

    js

---
+++ basicjs/operation.js
@@ -0,0 +1,13 @@
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4);
+console.log(10 % 3);
+
+console.log((10 + 3 - 5) * 10);
+
+console.log("sung" + " " + "bin");
+
+let userName = "Sungbin";
+console.log(userName.length);
